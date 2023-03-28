ea2aa5016839588643efe46eb83d2815eaed8d55
Pamir Perdeci <pamirperdeci@gmail.com>
2023-03-28 17:14:26 -0400

    created app and restart button mvp achieved in 16 minutes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [age, setAge] = useState("")
+  const changeHandler = (e) => {
+    setAge(e.target.value)
+    console.log(age)
+  }
+  const submitHandler =(e) => {
+    e.preventDefault();
+    const ageInDays = parseInt(age)*365;
+    const displayString = `You are ${ageInDays} days old or maybe more idk`
+    setAge(displayString)
+  }
 
+    const restartHandler = (e) => {
+      e.preventDefault();
+      setAge("")
+    }
   return (
     <div className="App">
       <h1>How old are you?</h1>
+      <h1>{age}</h1>
+      <form>
+
+        <input onChange={changeHandler} value={age} type="text"></input>
+        <button onClick={submitHandler}>Submit</button>
+      </form>
+      <button onClick={restartHandler}>Restart</button>
     </div>
   )
 }
